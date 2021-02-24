3502ced377a3fb25112770a79e839ed62b2b74dc
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2021-02-24 18:56:30 +0100

    Move implementation of bitset operations into header as macros

--- bitset.c
+++ bitset.c
@@ -3,13 +3,15 @@
 
 #include "bitset.h"
 
+/*
 typedef struct {
     unsigned char *set;
     int size;
 } bitset_t;
 
 typedef unsigned long bitset_index_t;
-
+*/
+/*
 void bitset_ctor(bitset_t *set, int bits) {
     int size = bits / 8 + 1;
     unsigned char *chars = malloc(size);
@@ -19,7 +21,9 @@ void bitset_ctor(bitset_t *set, int bits) {
     set->set = chars;
     set->size = size;
 }
+*/
 
+/*
 // gets value of the bit at index i
 // returns 0 or 1
 int  bitset_get(bitset_t *set, bitset_index_t i) {
@@ -29,7 +33,9 @@ int  bitset_get(bitset_t *set, bitset_index_t i) {
     int mask = 1 << (8 - bit - 1);
     return (set->set[byte] & mask) != 0;
 }
+*/
 
+/*
 void bitset_set(bitset_t *set, bitset_index_t i, int exp) {
     int byte = i / 8;
     int bit = i % 8;
@@ -42,11 +48,11 @@ void bitset_set(bitset_t *set, bitset_index_t i, int exp) {
         set->set[byte] &= mask;
     }
 }
+*/
 
 int main() {
 
-    bitset_t set;
-    bitset_ctor(&set, 100);
+    bitset_alloc(set, 100);
     set.set[0] = 0;
     set.set[1] = 0;
 
@@ -55,7 +61,7 @@ int main() {
 
     for (int i = 0; i < 16; i++) {
         if (i % 2 == 0) {
-            bitset_set(&set, i, 1);
+            bitset_setbit(set, i, 1);
         }
     }
 
@@ -63,9 +69,11 @@ int main() {
     printf("%d\n", set.set[1]);
 
     for (int i = 0; i < 16; i++) {
-        int val = bitset_get(&set, i);
+        int val = bitset_getbit(set, i);
         printf("%d", val);
     }
+
+    bitset_free(set)
 
     return 0;
 }
--- bitset.h
+++ bitset.h
@@ -1,9 +1,49 @@
 #ifndef BITSET_H_
 #define BITSET_H_
 
-//typedef <DOPLNIT> bitset_t;
+typedef struct {
+    unsigned char *set;
+    unsigned long size;
+} bitset_t;
 
 typedef unsigned long bitset_index_t;
 
+//TODO error handling
+#define bitset_alloc(jmeno_pole, velikost) \
+    bitset_t (jmeno_pole); \
+    int size = (velikost) / 8 + 1; \
+    unsigned char *chars = malloc(size); \
+    if (chars == NULL) { \
+        exit(EXIT_FAILURE); \
+    } \
+    (jmeno_pole).set = chars; \
+    (jmeno_pole).size = size; \
+
+#define bitset_free(jmeno_pole) \
+    free((jmeno_pole).set);
+
+# define bitset_size(jmeno_pole) (jmeno_pole.size)
+
+/*
+#define bitset_getbit(jmeno_pole, index) \
+    int byte = index / 8; \
+    int bit = index % 8; \
+    int mask = 1 << (8 - bit - 1); \
+    (jmeno_pole.set[byte] & mask) != 0; \
+*/
+
+#define bitset_setbit(jmeno_pole, index, vyraz) \
+    int byte = (index) / 8; \
+    int bit = (index) % 8; \
+    int mask = 1 << (8 - bit - 1); \
+    if (vyraz) { \
+        (jmeno_pole).set[byte] |= mask; \
+    } else { \
+        mask = ~mask; \
+        (jmeno_pole).set[byte] &= mask; \
+    } \
+
+#define bitset_getbit(jmeno_pole, index) \
+    (((jmeno_pole).set[(index) / 8] & (1 << (7 - (index) % 8))) != 0)
 
 #endif
